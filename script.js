c0ad9157618f8f2f90060989ef3c353f9ead24d3
Kostiantyn Rakhmanov <rahmanoff@gmail.com>
2023-07-16 16:32:20 +0300

    Added: responsive

--- script.js
+++ script.js
@@ -14,12 +14,12 @@ window.addEventListener('load', function () {
 
   // effect settings
   let size = canvas.width < canvas.height ? canvas.width * 0.25 : canvas.height * 0.25;
-  const maxLevel = 4;
+  const maxLevel = 7;
   const branches = 2;
 
-  let sides = 6;
-  let scale = 0.5;
-  let spread = 0.1;
+  let sides = 5;
+  let scale = 0.7;
+  let spread = 0.6;
   let color = 'hsl(' + Math.random() * 360 + ',100%, 50%)';
   let lineWidth = Math.floor(Math.random() * 20 + 10);
 
@@ -61,13 +61,11 @@ window.addEventListener('load', function () {
       drawBranch(level + 1);
       ctx.restore();
 
-      ctx.save();
-      ctx.rotate(-spread);
-      drawBranch(level + 1);
-      ctx.restore();
-
       ctx.restore();
     }
+    ctx.beginPath();
+    ctx.arc(0, size, size * 0.1, 0, Math.PI * 2);
+    ctx.fill();
   }
 
   function drawFractal() {
@@ -75,6 +73,7 @@ window.addEventListener('load', function () {
     ctx.save();
     ctx.lineWidth = lineWidth;
     ctx.strokeStyle = color;
+    ctx.fillStyle = color;
     ctx.translate(canvas.width / 2, canvas.height / 2);
     for (let i = 0; i < sides; i++) {
       ctx.rotate((Math.PI * 2) / sides);
@@ -88,7 +87,7 @@ window.addEventListener('load', function () {
 
   function randomizeFractal() {
     sides = Math.floor(Math.random() * 7 + 2);
-    scale = Math.random() * 0.2 + 0.4;
+    scale = Math.random() * 0.4 + 0.4;
     spread = Math.random() * 2.9 + 0.1;
     color = 'hsl(' + Math.random() * 360 + ',100%, 50%)';
     lineWidth = Math.floor(Math.random() * 20 + 10);
@@ -121,4 +120,15 @@ window.addEventListener('load', function () {
     label_sides.innerText = 'Sides:' + sides;
   }
   updateSliders();
+
+  window.addEventListener('resize', function () {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    size = canvas.width < canvas.height ? canvas.width * 0.25 : canvas.height * 0.25;
+    ctx.shadowColor = 'rgba(0,0,0,0.7)'
+    ctx.shadowOffsetX = 10;
+    ctx.shadowOffsetY = 5;
+    ctx.shadowBlur = 10;
+    drawFractal();
+  });
 });
